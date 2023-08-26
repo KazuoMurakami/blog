5a1cfad688323334a794a8221e806d670ff50b87
Octavio Kazuo <otaviokazuo12@hotmail.com>
2023-08-26 12:15:07 -0300

    Finish Section AboutMe

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,17 @@
+
 import Image from 'next/image'
 import Navbar from "@/components/Navbar"
 import SectionAbout from "@/components/SectionAbout"
+import Stack from '@/components/Stack'
 
 export default function Home() {
   return (
     <>
-       <Navbar/>
-       <SectionAbout/>
+    <div className='h-screen flex flex-col'>
+      <Navbar/>
+      <SectionAbout/>
+    </div>
+       <Stack/>
     </>
   )
 }
