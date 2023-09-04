5733c9ab205f202ea176716e78b4dd790a3abbb0
Octavio Kazuo <otaviokazuo12@hotmail.com>
2023-09-03 22:44:53 -0300

    alterações

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ export default function navbar(){
         <nav className="flex gap-6 justify-center items-center text-xl space-x-5 tracking-widest p-4">
             {Menu.map((item) => {
                 return (
-                    <Link href={item.root} className="border-black border-b-4 hover:border-blue-400"><li>{item.content}</li></Link>
+                    <Link key="1" href={item.root} className="border-black border-b-4 hover:border-blue-400"><li>{item.content}</li></Link>
                 )
             })}
             <a href="https://www.linkedin.com/in/octavio-murakami-4696561a3/" target="_blank"><img src="/images/linkedin.svg" alt="" height={60} width={60}/></a>
